1905a04ade23ac2002256fb7cd4a2d2c49ba36a3
ClassyLobster <55981867+ClassyLobster@users.noreply.github.com>
2022-04-02 11:49:34 +0300

    Wiggle wiggle wiggle

--- src/game-scene.js
+++ src/game-scene.js
@@ -1,9 +1,25 @@
 export class GameScene extends Phaser.Scene {
+    constructor() {
+        super('GameScene');
+        this.move_speed = 5;
+    }
+
     preload() {
         this.load.image('geekythor', 'assets/geekythor.jpg');
     }
 
     create() {
-        this.add.image(400, 300, 'geekythor');
+        this.geeky_thor = this.add.image(400, 300, 'geekythor');
+        this.up_key = this.input.keyboard.addKey('up');
+        this.down_key = this.input.keyboard.addKey('down');
+        this.left_key = this.input.keyboard.addKey('left');
+        this.right_key = this.input.keyboard.addKey('right');
+    }
+
+    update() {
+        this.geeky_thor.y -= this.input.keyboard.checkDown(this.up_key) ? this.move_speed : 0;
+        this.geeky_thor.y += this.input.keyboard.checkDown(this.down_key) ? this.move_speed : 0;
+        this.geeky_thor.x -= this.input.keyboard.checkDown(this.left_key) ? this.move_speed : 0;
+        this.geeky_thor.x += this.input.keyboard.checkDown(this.right_key) ? this.move_speed : 0;
     }
 }
