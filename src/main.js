016ba4f8f2a91171a073db4debe878d587c66ee6
Bulbul Akabulbul <galsol02@gmail.com>
2022-04-03 13:18:23 +0300

    Added main menu

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
 const Phaser = require('phaser');
 const { GameScene } = require('./game-scene.js');
 const Consts = require('./consts.js');
+import { MainMenuScene } from './main-menu-scene.js';
 
 const gameConfig = {
     type: Phaser.AUTO,
     parent: 'content',
     physics: {
         default: 'arcade',
-        arcade: { debug:false }
+        arcade: { debug: false }
     },
     width: Consts.CANVAS_WIDTH,
     height: Consts.CANVAS_HEIGHT,
-    scene: [ GameScene ]
+    scene: [MainMenuScene, GameScene]
 }
 
 const game = new Phaser.Game(gameConfig);
